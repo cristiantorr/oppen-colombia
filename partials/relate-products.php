96aeeac710c89848332245fcf5d4bd21dece515d
cristiantorr <ps4cristiantorr@gmail.com>
2025-11-07 16:12:37 -0500

    Feat: add partials

--- partials/relate-products.php
+++ partials/relate-products.php
@@ -16,7 +16,7 @@ if ($relatedProducts): ?>
   <div class="relative">
     <div class="swiper swiper-relacionados">
       <div class="swiper-wrapper">
-        <div class="swiper-slide">
+        <!--         <div class="swiper-slide">
           <article class="card-product category-vida">
             <figure>
               <img src="<?php echo get_stylesheet_directory_uri() . '/library/' ?>images/producto-1.jpg" alt="">
@@ -105,23 +105,35 @@ if ($relatedProducts): ?>
               <h6>Bafle Mini Bluetooth Zip Cork</h6>
             </div>
           </article>
-        </div>
-        <?php /* foreach ($relatedProducts as $relatedProduct): $relatedProductsCat = get_the_terms($relatedProduct->ID, 'products_cat'); ?>
+        </div> -->
+        <?php foreach ($relatedProducts as $index => $relatedProduct): $relatedProductsCat = get_the_terms($relatedProduct->ID, 'products_cat');
+
+          $productsCat = get_the_terms($relatedProduct->ID, 'products_cat');
+          if ($productsCat && !is_wp_error($productsCat)) {
+            $firstCat = $productsCat[0];
+            $taxcolor = get_field('tax_color', 'term_' . $firstCat->term_id);
+          }
+        ?>
 
           <div class="swiper-slide">
-            <article class="card-product category-vida">
+            <article class="card-product category-vida category-<?php echo esc_attr($index); ?>">
               <figure>
                 <?php if (has_post_thumbnail()): $featuredimgurl = get_the_post_thumbnail_url($relatedProduct->ID, 'bones-thumb-528'); ?>
 
                   <img src="<?php echo esc_url($featuredimgurl); ?>" alt="<?php the_title_attribute(); ?>">
                 <?php endif; ?>
                 <div class="overflow">
-                  <a href="#" class="btn-asesor">Contactar asesor</a>
+                  <a href="https://wa.me/+573218033728" class="btn-asesor">Contactar asesor</a>
                   <div class="details">
                     <a href="<?php echo get_permalink($relatedProduct->ID); ?>" class="btn-product">Ver producto</a>
                     <h4><?php echo $relatedProduct->post_title; ?></h4>
                   </div>
                 </div>
+                <style>
+                  .card-product.category-<?php echo esc_attr($index); ?>:hover figure {
+                    border-color: <?php echo esc_attr($taxcolor); ?>
+                  }
+                </style>
               </figure>
               <div class="info">
                 <?php if (!empty($relatedProductsCat) && !is_wp_error($relatedProductsCat)): $category = wp_list_pluck($relatedProductsCat, 'name'); ?>
@@ -133,7 +145,7 @@ if ($relatedProducts): ?>
               </div>
             </article>
           </div>
-        <?php endforeach; */ ?>
+        <?php endforeach;  ?>
       </div>
     </div>
     <div class="swiper-button-next next-relacionados"></div>
