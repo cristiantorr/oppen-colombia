55235697ac5dee428f0af133e6b3bb66024c9592
cristiantorr <ps4cristiantorr@gmail.com>
2025-11-16 12:59:53 -0500

    Feat: Add library

--- library/bones.php
+++ library/bones.php
@@ -250,40 +250,12 @@ function bones_theme_support()
 		'search-form',
 		'comment-form'
 	));
+
+	add_theme_support('custom-logo');
 } /* end bones theme support */
 
 
-/*********************
-RELATED POSTS FUNCTION
- *********************/
 
-// Related Posts Function (call using bones_related_posts(); )
-function bones_related_posts()
-{
-	echo '<ul id="bones-related-posts">';
-	global $post;
-	$tags = wp_get_post_tags($post->ID);
-	if ($tags) {
-		foreach ($tags as $tag) {
-			$tag_arr .= $tag->slug . ',';
-		}
-		$args = array(
-			'tag' => $tag_arr,
-			'numberposts' => 5, /* you can change this to show more */
-			'post__not_in' => array($post->ID)
-		);
-		$related_posts = get_posts($args);
-		if ($related_posts) {
-			foreach ($related_posts as $post) : setup_postdata($post); ?>
-				<li class="related_post"><a class="entry-unrelated" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-			<?php endforeach;
-		} else { ?>
-			<?php echo '<li class="no_related_post">' . __('No Related Posts Yet!', 'bonestheme') . '</li>'; ?>
-<?php }
-	}
-	wp_reset_postdata();
-	echo '</ul>';
-} /* end bones related posts function */
 
 /*********************
 PAGE NAVI
@@ -328,7 +300,3 @@ function bones_excerpt_more($more)
 	// edit here if you like
 	return '...  <a class="excerpt-read-more" href="' . get_permalink($post->ID) . '" title="' . __('Read ', 'bonestheme') . esc_attr(get_the_title($post->ID)) . '">' . __('Read more &raquo;', 'bonestheme') . '</a>';
 }
-
-
-
-?>
